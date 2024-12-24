c4f7197133181a3f53d98e6441084c938df909a5
Umarov Rahmatillo <rahmatillo722022@gmail.com>
2024-12-24 22:35:34 +0500

    chartlarning barisi firebasedan to'g'ri holatda kelyapti va mos holatda chartlar chizilyapti

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore, collection, getDocs } from "firebase/firestore"; // Firestore
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_apiKey,
@@ -11,17 +10,9 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_appId,
 };
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAXivWRoFPKFGiwaYhglPooEm5rG72bJXg",
-//   authDomain: "screener-2b456.firebaseapp.com",
-//   projectId: "screener-2b456",
-//   storageBucket: "screener-2b456.firebasestorage.app",
-//   messagingSenderId: "860474515607",
-//   appId: "1:860474515607:web:0b0cc8f4d3d5d757aa49d2"
-// };
+
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
-export { db, collection, getDocs };
+export { db };
