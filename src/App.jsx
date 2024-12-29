08c84dd176fb9be12b89b9c562091ba529923108
yasin-frontend <yasin2009revenge@gmail.com>
2024-12-29 16:15:10 +0500

    pull2

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,6 @@ const App = () => {
   const [selectValues, setSelectValues] = useState(null);
   const [screeningTypeValueId, setScreeningTypeValueId] = useState(null)
   const [timeFrameId, setTimeFrameId] = useState(null)
-
   const [foundTimeId, setFoundTimeId] = useState("");
   const [isGrid, setIsGrid] = useState(6);
   const [isCard, setIsCard] = useState(false);
