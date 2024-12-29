88bffda6c1d45d2b49b9418a0a2e2101aed3b887
yasin-frontend <yasin2009revenge@gmail.com>
2024-12-29 16:12:15 +0500

    deleted word 'salom'

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,7 @@ const App = () => {
   const [screeningTypeValueId, setScreeningTypeValueId] = useState(null)
   const [timeFrameId, setTimeFrameId] = useState(null)
 
-
-  const [foundTimeId, setFoundTimeId] = useState("salom");
+  const [foundTimeId, setFoundTimeId] = useState("");
   const [isGrid, setIsGrid] = useState(6);
   const [isCard, setIsCard] = useState(false);
   const [isAlert, setIsAlert] = useState(false);
